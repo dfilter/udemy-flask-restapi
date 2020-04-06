b89c9c5ce7946e0b8a1fba050beec54b55c243ce
dfilter <dirk_mail@protonmail.com>
2020-04-06 15:43:56 -0400

    21 list comprehension

---
+++ section-2/21-list-comprehension.py
@@ -0,0 +1,20 @@
+numbers = [1, 3, 5]
+doubled = []
+
+for number in numbers:
+    doubled.append(number * 2)
+
+# Same as above
+doubled = [number * 2 for number in numbers]
+print(doubled)
+
+friends = ['Rolf', 'Sam', 'Samantha', 'Saurabh', 'Jen']
+starts_s = []
+
+for friend in friends:
+    if friend.startswith('S'):
+        starts_s.append(friend)
+
+# same as above loop
+starts_s = [friend for friend in friends if friends.startswith('S')]
+print(starts_s)
