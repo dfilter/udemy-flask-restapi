6652ac5292682799561ea11a4fc8b8707158e814
dfilter <dirk_mail@protonmail.com>
2020-04-07 12:38:18 -0400

    47 simple decorators

---
+++ section-2/47-simple-decorators.py
@@ -0,0 +1,17 @@
+user = {'name': 'Jose', 'access_level': 'admin'}
+
+def get_admin_password():
+    return '1234'
+
+def make_secure(func):
+    def secure_function():
+        if user['access_level'] == 'admin':
+            return func()
+        else:
+            return f'No Admin Permission for user {user["name"]}'
+    
+    return secure_function
+
+
+get_admin_password = make_secure(get_admin_password)
+print(get_admin_password())
