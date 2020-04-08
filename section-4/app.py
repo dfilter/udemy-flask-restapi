17ea354a0b8abf296a0c03f82f4ed7d4822d4fe0
dfilter <dirk_mail@protonmail.com>
2020-04-08 11:13:23 -0400

    75 Authenticateion part 1

--- section-4/app.py
+++ section-4/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.secret_key = 'secret-key'
+
 api = Api(app)
 
 items = []
@@ -17,8 +19,8 @@ class Item(Resource):
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        if next(filter(lambda item: item['name'] == name, items),
-                None) is not None:
+        item = next(filter(lambda item: item['name'] == name, items), None)
+        if item is not None:
             return {
                 'message':
                 "An items with name '{}' already exists.".format(name)
