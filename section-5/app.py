6e8d328a89e7b9ffa70bcdb6b203da84ad4279ff
dfilter <dirk_mail@protonmail.com>
2020-04-08 14:27:53 -0400

    88 preventing duplicate usernames

--- section-5/app.py
+++ section-5/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_jwt import JWT, jwt_required
 from flask_restful import Resource, Api, reqparse
 
+from user import UserRegister
 from security import authenticate, identity
 
 app = Flask(__name__)
@@ -70,5 +71,6 @@ class Items(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Items, '/items')
+api.add_resource(UserRegister, '/register')
 
 app.run(port=5000, debug=True)
--- section-5/user.py
+++ section-5/user.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 
+from flask_restful import Resource, reqparse
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 database_location = os.path.join(dir_path, 'data.db')
@@ -24,7 +25,7 @@ class User:
             user = cls(*row)
         else:
             user = None
-        
+
         connection.close()
         return user
 
@@ -40,6 +41,35 @@ class User:
             user = cls(*row)
         else:
             user = None
-        
+
         connection.close()
         return user
+
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help='This field is required.')
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help='This field is required.')
+    
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if User.find_by_username(data['username']):
+            return {'message': 'A user with that username already exists.'}, 400
+
+        connection = sqlite3.connect(database_location)
+        cursor = connection.cursor()
+
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username'], data['password']))
+
+        connection.commit()
+        connection.close()
+
+        return {'message': 'User created successfully.'}, 201
