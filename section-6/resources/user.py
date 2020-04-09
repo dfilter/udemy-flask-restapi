4f9404f6cedd7739f9d85ebb64f580d53e469f1b
dfilter <dirk_mail@protonmail.com>
2020-04-09 10:16:21 -0400

    107 implimenting usermodel with sqlalchemy

--- section-6/resources/user.py
+++ section-6/resources/user.py
@@ -23,16 +23,10 @@ class UserRegister(Resource):
     def post(self):
         data = UserRegister.parser.parse_args()
 
-        if User.find_by_username(data['username']):
+        if UserModel.find_by_username(data['username']):
             return {'message': 'A user with that username already exists.'}, 400
 
-        connection = sqlite3.connect(database_location)
-        cursor = connection.cursor()
-
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
+        user = UserModel(**data)
+        user.save_to_db()
 
         return {'message': 'User created successfully.'}, 201
